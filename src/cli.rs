fbe4b905a5aee459e599972da3a47d0f2234ec92
pjmp <pomba.magar@gmail.com>
2020-04-20 18:14:19 +0545

    chore: remove `clap` from deps

--- src/cli.rs
+++
@@ -1,8 +0,0 @@
-use clap::{crate_description, crate_name, crate_version, App, AppSettings};
-
-pub fn new() -> App<'static, 'static> {
-    App::new(crate_name!())
-        .version(crate_version!())
-        .about(crate_description!())
-        .setting(AppSettings::ColorAuto)
-}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,44 @@
-mod cli;
+use std::env::args;
+
 mod ikill;
+
+const USAGE: &str = "
+ikill - Interactively kill processes
+
+USAGE:
+    ikill
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+";
+
+fn unknown_args(arg: String) {
+    println!("
+error: Found argument '{}' which wasn't expected, or isn't valid in this context
+
+USAGE:
+    ikill
+
+For more information try --help
+", arg);
+}
 
 #[tokio::main]
 async fn main() {
-    cli::new().get_matches();
-
-    ikill::run().await;
+    match args().nth(1) {
+        None => {
+            ikill::run().await;
+        },
+        Some(arg) => {
+            if arg == "-h" || arg == "--help" {
+                println!("{}", USAGE);
+            } else if arg == "-V" || arg == "--version" {
+                println!("ikill v1.2.1");
+            } else {
+                unknown_args(arg);
+                std::process::exit(1);
+            }
+        },
+    };
 }
